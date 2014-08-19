8cd6a571d10f9d93ad151db04cdbb59f7de619fe
ChrisTimperley <christimperley@googlemail.com>
2014-08-19 02:13:43 +0100

    Fixed bugs in condition transformer and added redundant bracket removal.

--- lib/transformers/condition_transformer.rb
+++ lib/transformers/condition_transformer.rb
@@ -6,9 +6,21 @@ class RubyPlusPlus::ConditionTransformer < RubyPlusPlus::Transformer
 
   def transform(code)
     #/(if\s+[^\(\n\)]+)/
-    code.gsub(/\b(if|elsif|else if|while|for|until|unless)\s+[^\(\n\)]+/) do |statement|
+    #/\b(if|elsif|while|for|until|unless)\s+[^\(\n\)]+/
+    code.gsub(/\b(if|elsif|while|for|until|unless)\s+[^\n]+/) do |statement|
+
+      # Extract the statement type keyword and the condition.
       type, condition = statement.split(/\s/, 2)
-      "#{type} (#{condition.strip})"
+      condition = condition.strip
+
+      # To avoid redundancy, remove any brackets enclosing the entire condition.
+      if condition[0] == '(' && find_closing_bracket(condition, 0) == condition.length - 1
+        condition = condition[1 ... -1]
+      end
+
+      # Reformat to ensure that the condition is enclosed by a single pair of parentheses.
+      "#{type} (#{condition})"
+
     end
   end
 
